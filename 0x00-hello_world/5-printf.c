f20e3af48f75a25bc1a3d333fc09d86e5930da9f
root <kbdiky@gmail.com>
2023-02-17 15:15:57 -0800

    using the print function in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A C program that prints out usiing the printf function
+ * description - this is different form the previous tasks using puts
+ *
+ * Return: Good job 0
+ */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
