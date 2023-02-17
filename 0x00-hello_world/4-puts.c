80f6ef28e33d82634216f5db713dd1b6ad723f0b
root <kbdiky@gmail.com>
2023-02-17 12:10:51 -0800

    betty style of coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+include < stdio.h >
+/**
+ * main - Using the puts function to print outputn on the screen
+ * The program seeks to show how to use the puts function in C
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
