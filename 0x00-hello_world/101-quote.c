0b45896e62ad412c5fe5f02797e32c4e12b5cfd9
root <kbdiky@gmail.com>
2023-02-19 11:04:02 -0800

    printing on the standard output stream using fputs

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio>
+#include <unistd>
+
+/**
+ * main - This is a Program that uses fputs to print
+ * output stream.
+ *
+ * Description - the script allows access into the POSIX
+ * for unix-like systems and compilers.
+ * Return: Success 1
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" -
+	Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
